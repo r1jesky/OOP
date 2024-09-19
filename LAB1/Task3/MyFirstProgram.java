97c646368b0ef9c051c666d9a79f7764a17c858f
r1jesky <gggggbgfrhxt@gmail.com>
2024-09-19 12:19:53 +0400

    main upd

--- LAB1/Task3/MyFirstProgram.java
+++ LAB1/Task3/MyFirstProgram.java
@@ -1,7 +1,16 @@
 class MyFirstClass {
     public static void main(String[] s) {
-		for (int i = 0; i < s.length; i++) {
-			System.out.println(s[i]);
+		MySecondClass o = MySecondClass(3,8);
+		System.out.println(o.max());
+		for(int i = 1; i <= 8; i++){
+			for(int j = 1; j <= 8; j++){
+				o.setValue1(i);
+				o.setValue2(j);
+				
+				System.out.println(o.max());
+				System.out.print(" ");
+			}
+			System.out.println();
 		}
 	}
 }
