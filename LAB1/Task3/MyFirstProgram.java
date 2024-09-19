edf3a1691a272d8adb124d6025fe978fd6663398
r1jesky <gggggbgfrhxt@gmail.com>
2024-09-19 12:12:50 +0400

    add second class

---
+++ LAB1/Task3/MyFirstProgram.java
@@ -0,0 +1,38 @@
+class MyFirstClass {
+    public static void main(String[] s) {
+		for (int i = 0; i < s.length; i++) {
+			System.out.println(s[i]);
+		}
+	}
+}
+
+class MySecondClass {
+	private int value1;
+	private int value2;
+	
+	public int max(){
+		if(value1>value2) return value1;
+		else return value2;
+	}
+
+	public MySecondClass(int value1, int value2){
+		this.value1=value1;
+		this.value2=value2;
+	}
+
+	public int getValue1(){
+		return value1;
+	}
+
+	public int getValue2(){
+		return value2;
+	}
+
+	public void setValue1(int value1){
+		this.value1=value1;
+	}
+
+	public void setValue2(int value2){
+		this.value2=value2;
+	}
+}
