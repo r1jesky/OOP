5aad969bb750cf14ab5dee424b6b7738129627ed
r1jesky <gggggbgfrhxt@gmail.com>
2024-09-26 18:50:33 +0400

    class switched

--- LAB1/Task4/MyFirstProgram.java
+++ LAB1/Task4/MyFirstProgram.java
@@ -2,7 +2,7 @@ import myfristpackage.*;
 
 class MyFirstClass {
     public static void main(String[] s) {
-		MySecondClass o = MySecondClass(3,8);
+		MyFirstClass o = MyFirstClass(3,8);
 		System.out.println(o.max());
 		for(int i = 1; i <= 8; i++){
 			for(int j = 1; j <= 8; j++){
